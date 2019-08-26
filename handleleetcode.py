1c51398b937926b4ec233bf2c5923ad1d0cc637d
iamslash <iamslash@gmail.com>
2019-08-26 22:51:17 +0900

    update signin

--- handleleetcode.py
+++ handleleetcode.py
@@ -1,10 +1,21 @@
 # -*- coding: utf-8 -*-
 
+# references:
+#  https://github.com/jrluu/Leetcode-Scraper/blob/master/leetcode_scraper.py
+
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+import time
 import pprint, random
 
+
 l_key = ['leetcode', u'릿코드', u'문제']
+TIME_DELAY = 2
 
 def probe(d_pkt):
     text = d_pkt['text']
@@ -26,8 +37,16 @@ def makeDriver():
 
 def printByTag(driver, tag):
     url_by_tag = 'https://leetcode.com/problemset/all/?topicSlugs={0}'.format(tag.lower())    
+
     print('requesting url: {0} ...'.format(url_by_tag))
+    wait = WebDriverWait(driver, 10)
     driver.get(url_by_tag)
+
+    # click freq button
+    print('clicking...')
+    btn_freq = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "reactable-th-frequency")))
+    btn_freq.click()
+
     # process final html
     # l_el = driver.find_elements_by_xpath("//a[contains(@href, '/problems/')]")
     l_el  = driver.find_element_by_xpath('//*[@id="question-app"]/div/div[2]/div[2]/div[2]/table/tbody[1]')
@@ -36,18 +55,20 @@ def printByTag(driver, tag):
         print('[{0}]({1})'.format(el.text, el.get_attribute("href")))   
 
 def clickFreq(driver):
-    url_tgt = r'//*[@id="question-app"]/div/div[2]/div[2]/div[2]/table/thead/tr/th[7]'
-    url_tgt = r'//*[@id="question-app"]/div/div[2]/div[2]/div[2]/table/thead/tr/th[6]'
-    el   = driver.find_element_by_xpath(url_tgt)
-    print(el)
-    # el.click()
+    driver.get('https://leetcode.com/problemset/all')
+
+    el   = driver.find_element_by_class_name(r'reactable-th-frequency')
+    el.click()
 
 def getTags(driver):
     driver.get('https://leetcode.com/problemset/all')
-
-    # l_el = driver.find_elements(By.CSS_SELECTOR, '.tag-category')
-    l_el = driver.find_elements_by_css_selector('.tag-category')
-    print(l_el)
+    
+    l_el = driver.find_elements_by_xpath(r'//div/div/div/div[2]/div[1]/span')
+    #l_el = driver.find_elements_by_class_name('filter-dropdown-menu-item')    
+    # pprint.pprint(l_el)
+    for el in l_el:
+        el2 = el.find_element_by_tag_name('span')
+        print(el2.text)
 
 def pickone(difficulty='Medium'):
     # url_prob_desc = 'https://leetcode.com/problems/{0}/description/'
@@ -77,8 +98,55 @@ def pickone(difficulty='Medium'):
         link += '/description/'
     return link
 
+def sign_into_leetcode(driver):
+    driver.implicitly_wait(TIME_DELAY)
+
+    driver.get("https://leetcode.com/accounts/logout")
+
+    valid_choice = False
+
+    while (not valid_choice):
+        valid_choice = True
+
+        option = input("Type in method to login \n 1. Leetcode \n 2. Facebook \n 3. Google \n 4. Linkedin \n 5. Github \n")
+        username = input("Enter your username: ")
+        password = input("Enter your password: ")
+
+        if option == "1":
+            driver.get("https://leetcode.com/accounts/login/")            
+            driver.find_element_by_xpath('//*[@id="app"]/div/div[2]/div/div[2]/div/div/div/form/span[1]/input').send_keys(username)
+            driver.find_element_by_xpath('//*[@id="app"]/div/div[2]/div/div[2]/div/div/div/form/span[2]/input').send_keys(password)
+            driver.find_element_by_xpath('//*[@id="app"]/div/div[2]/div/div[2]/div/div/div/form/span[2]/input').send_keys(Keys.ENTER)
+        elif option == '2':
+            driver.get("https://leetcode.com/accounts/facebook/login/")
+            driver.find_element_by_xpath('// *[ @ id = "email"]').send_keys(username)
+            driver.find_element_by_xpath('//*[@id="pass"]').send_keys(password)
+            driver.find_element_by_xpath('//*[@id="pass"]').send_keys(Keys.ENTER)
+        elif option == '3':
+            driver.get("https://leetcode.com/accounts/google/login/")
+            driver.find_element_by_xpath('// *[ @ id = "Email"]').send_keys(username)
+            driver.find_element_by_xpath('// *[ @ id = "Email"]').send_keys(Keys.ENTER)
+            driver.find_element_by_xpath('//*[@id="Passwd"]').send_keys(password)
+            driver.find_element_by_xpath('//*[@id="Passwd"]').send_keys(Keys.ENTER)
+        elif option == '4':
+            driver.get("https://leetcode.com/accounts/linkedin/login/")
+            driver.find_element_by_name('session_key').send_keys(username)
+            driver.find_element_by_name('session_password').send_keys(password)
+            driver.find_element_by_name('session_password').send_keys(Keys.ENTER)
+        elif option == '5':
+            driver.get("https://leetcode.com/accounts/github/login/")
+            driver.find_element_by_xpath('// *[ @ id = "login_field"]').send_keys(username)
+            driver.find_element_by_xpath('//*[@id="password"]').send_keys(password)
+            driver.find_element_by_xpath('//*[@id="password"]').send_keys(Keys.ENTER)
+        else:
+            valid_choice = False
+            print("Invalid Choice. Please choose a number from 1 to 5 \n")
+
+    driver.implicitly_wait(0)
+
+
 if __name__ == "__main__":
     # print(pickone())
     driver = makeDriver()
-    # clickFreq(driver)
-    getTags(driver)
+    sign_into_leetcode(driver)
+    printByTag(driver, 'array')
