1acf0e068a8fb1b24596b5c84306fc99c8dcc3fb
David Sun <iamslash@gmail.com>
2019-08-26 12:41:21 +0900

    update print by tag

--- handleleetcode.py
+++ handleleetcode.py
@@ -16,8 +16,31 @@ def probe(d_pkt):
 def handle(d_pkt):
     return pickone()
 
+def makeDriver():
+    options = webdriver.ChromeOptions()
+    # options.binary_location = '/usr/local/bin/chromedriver'
+    options.add_argument('headless')
+    driver = webdriver.Chrome(chrome_options=options)
+    driver.implicitly_wait(3)
+    return driver
+
+def printByTag(driver, tag):
+    url_by_tag = 'https://leetcode.com/problemset/all/?topicSlugs={0}'.format(tag.lower())    
+    print('requesting url: {0} ...'.format(url_by_tag))
+    driver.get(url_by_tag)
+    # process final html
+    # l_el = driver.find_elements_by_xpath("//a[contains(@href, '/problems/')]")
+    l_el  = driver.find_element_by_xpath('//*[@id="question-app"]/div/div[2]/div[2]/div[2]/table/tbody[1]')
+    l_els = l_el.find_elements_by_xpath("//a[contains(@href, '/problems/')]")
+    for el in l_els:
+        print('[{0}]({1})'.format(el.text, el.get_attribute("href")))   
+
+def clickFreq(driver):
+    el   = driver.find_element_by_xpath('//*[@id="question-app"]/div/div[2]/div[2]/div[2]/table/thead/tr/th[7]')
+    el.click()
+
 def pickone(difficulty='Medium'):
-    url_prob_desc = 'https://leetcode.com/problems/{0}/description/'
+    # url_prob_desc = 'https://leetcode.com/problems/{0}/description/'
     url_all_medium = 'https://leetcode.com/problemset/all/?difficulty={0}'.format(difficulty)
     
     options = webdriver.ChromeOptions()
@@ -45,4 +68,7 @@ def pickone(difficulty='Medium'):
     return link
 
 if __name__ == "__main__":
-    print(pickone())
+    # print(pickone())
+    driver = makeDriver()
+    clickFreq(driver)
+    printByTag(driver, "array")
